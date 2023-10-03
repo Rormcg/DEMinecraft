358c269a8ebe9e991f14e97a3847542bc8359049
Rory McGuire <rory.mcguire25@bishopireton.org>
2023-10-03 10:06:02 -0400

    Created scaffolding for Face

--- Minecraft/src/Face.java
+++ Minecraft/src/Face.java
@@ -2,8 +2,38 @@
  * Represents a face of a Shape3D
  */
 
+import java.awt.Graphics;
+import java.awt.Color;
 
 public class Face {
+	private RVector3D[] points;
 	
+	Face() {
+		points = null;
+	}
+	
+	Face(RVector3D[] p) {
+		points = p;
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public RVector3D[] getPoints() {
+		return points;
+	}
+	
+	public void setPoints(RVector3D[] p) {
+		points = p;
+	}
+	
+	public String toString() {
+		return "";
+	}
 	
 }
